1a33b0bfc15239ee3f1ad8ff10753bc0a33c0e7f
kevin lopes albuquerque <kevinup13@gmail.com>
2022-11-17 16:14:28 -0400

    alteracao

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function subirTela() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
+
+function btnScroll() {
+    if (window.scrollY === 0 || window.scrollY > 2600) {
+        //ocutar o btn
+        document.querySelector('.scrollbutton').style.display = 'none';
+    } else {
+        // mostrar o btn
+        document.querySelector('.scrollbutton').style.display = 'block';
+    }
+}
+window.addEventListener('scroll', btnScroll);
+
