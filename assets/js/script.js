735cee857816c653f11e5e9d38e66e36d44e527b
kevin lopes albuquerque <kevinup13@gmail.com>
2022-11-25 02:07:01 -0400

    alteracao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,9 @@
-/* function subirTela() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-};
-
-function btnScroll() {
-    if (window.scrollY === 0 || window.scroll.length > 2600) {
-        //ocutar o btn
-        document.querySelector('.scrollbutton').style.display = 'none';
-    } else {
-        // mostrar o btn
-        document.querySelector('.scrollbutton').style.display = 'block';
-    }
-
-}
-window.addEventListener('scroll', btnScroll);
-
- */
 function menuToggle() {
     let menuArea = document.getElementById('menu');
 
     if (menuArea.classList.contains('menu-opened') == true) {
         menuArea.classList.remove('menu-opened');
+
     } else {
         menuArea.classList.add('menu-opened');
     }
