5a08e8562c1dbf7f0752019e2b192c9c55eba446
kevin lopes albuquerque <kevinup13@gmail.com>
2022-11-25 01:29:36 -0400

    alteracao no js, menuToggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,13 @@ function btnScroll() {
 window.addEventListener('scroll', btnScroll);
 
  */
+function menuToggle() {
+    let menuArea = document.getElementById('menu');
+
+    if (menuArea.classList.contains('menu-opened') == true) {
+        menuArea.classList.remove('menu-opened');
+    } else {
+        menuArea.classList.add('menu-opened');
+    }
+    /* document.getElementById('menu').classList.add('menu-opened') */
+}
